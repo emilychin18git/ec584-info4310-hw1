7169d4f2e040e577fd5fac7c9b07a61b4c97fd17
Emily Chin <ec584@cornell.edu>
2020-09-15 20:40:52 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+# app.py
+from flask import *
+from whitenoise import WhiteNoise
+
+app = Flask(__name__)
+app.wsgi_app = WhiteNoise(app.wsgi_app, root='static/', prefix='static/', index_file="index.htm", autorefresh=True)
+
+
+@app.route('/', methods=['GET'])
+def hello():
+    return make_response("Go to /static/ for HW 1")
+    
+
+if __name__ == "__main__":
+    app.run(threaded=True, port=5000)
